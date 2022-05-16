d4803cde99837fa5e6bac7a1df885febd11713c6
Brodie <brodie.mickleson@gmail.com>
2022-05-17 08:10:47 +1200

    tutorial close. please start a new one.

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,15 @@ async function main () {
     // Retrieve accounts from the local node
     const accounts = await ethers.provider.listAccounts();
     console.log(accounts);
+
+    // Set up an ethers contract, representing our deployed Box instance
+    const address = '0x70997970c51812dc3a010c7d01b50e0d17dc79c8';
+    const Box = await ethers.getContractFactory('Box');
+    const box = await Box.attach(address);
+
+    // Call the retrieve() function of the deployed Box contract
+    const value = await box.retrieve();
+    console.log('Box value is', value.toString());
 }
 
 main()
